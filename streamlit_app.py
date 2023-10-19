0194e6668a8327c9f12c61fc75f31a33a6dcdfde
jomorbeta <143152074+jomorbeta@users.noreply.github.com>
2023-10-19 22:32:07 +0800

    Update streamlit_app.py
    Additional information about fresh cotton leaf and plant.

--- streamlit_app.py
+++ streamlit_app.py
@@ -79,8 +79,8 @@ disease_info = {
     'cotton': {
     'diseased cotton leaf': "Diseased cotton leaves often show symptoms like yellowing and wilting, which can be caused by various pathogens like Fusarium and Verticillium wilt, leading to a decline in cotton plant health and yield. Regular monitoring of cotton fields for early signs of leaf diseases is crucial for effective disease management.",
     'diseased cotton plant': "Cotton plant diseases can lead to reduced yield and fiber quality, affecting the overall cotton crop production. These diseases can be exacerbated by environmental conditions such as high humidity and insufficient air circulation. Implementing proper crop rotation, pest control, and maintaining healthy soil can help mitigate the risks associated with cotton plant diseases.",
-    'fresh cotton leaf': "Your cotton leaf appears healthy!",
-    'fresh cotton plant': "Your cotton plant appears healthy!",
+    'fresh cotton leaf': "Your cotton leaf appears healthy! In a thriving state, a fresh cotton leaf showcases vibrant green color, smooth texture, and well-defined veins. This is a positive sign of a leaf that is actively contributing to the plant's photosynthesis and overall well-being.",
+    'fresh cotton plant': "Your cotton plant appears healthy! A healthy cotton plant exhibits robust growth with lush, green foliage. It stands tall with sturdy stems, indicating a well-nourished and disease-free condition. Proper care has contributed to the plant's vitality and potential for optimal cotton production.",
     },
     'potato': {
         'Potato___Early_blight': "Early blight in potatoes can cause dark lesions on leaves. It is primarily caused by the fungus Alternaria solani. The symptoms include small, dark spots with concentric rings that may merge as the disease progresses. ",
